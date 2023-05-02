5521e4282ed262dbabcd86c11a918386aa8cb41d
billbobphil <37590467+billbobphil@users.noreply.github.com>
2023-05-01 20:53:14 -0700

    Added additional art including art to decorate the playing area

--- Assets/Scripts/General/WallGenerator.cs
+++ Assets/Scripts/General/WallGenerator.cs
@@ -5,6 +5,14 @@ namespace General
     public class WallGenerator : MonoBehaviour
     {
         public GameObject wallPrefab;
+        [SerializeField] private Sprite topWallSprite;
+        [SerializeField] private Sprite bottomWallSprite;
+        [SerializeField] private Sprite leftWallSprite;
+        [SerializeField] private Sprite rightWallSprite;
+        [SerializeField] private Sprite topRightCornerWallSprite;
+        [SerializeField] private Sprite topLeftCornerWallSprite;
+        [SerializeField] private Sprite bottomRightCornerWallSprite;
+        [SerializeField] private Sprite bottomLeftCornerWallSprite;
 
         public void GenerateWalls(float xMin, float xMax, float yMin, float yMax)
         {
@@ -13,17 +21,52 @@ namespace General
             
             for(float i = xMin; i < xMax; i++)
             {
-                Instantiate(wallPrefab, new Vector3(i, yMin, 0), Quaternion.identity).transform.SetParent(wallFolder.transform);
-                Instantiate(wallPrefab, new Vector3(i, yMax, 0), Quaternion.identity).transform.SetParent(wallFolder.transform);
+                GameObject bottomWall = Instantiate(wallPrefab, new Vector3(i, yMin, 0), Quaternion.identity); 
+                bottomWall.transform.SetParent(wallFolder.transform);
+                
+                if (i == xMin)
+                {
+                    bottomWall.GetComponent<SpriteRenderer>().sprite = bottomLeftCornerWallSprite;
+                    bottomWall.GetComponent<SpriteRenderer>().sortingOrder = 1;
+                }
+                else
+                {
+                    bottomWall.GetComponent<SpriteRenderer>().sprite = bottomWallSprite;    
+                }
+                
+                GameObject topWall = Instantiate(wallPrefab, new Vector3(i, yMax, 0), Quaternion.identity);
+                topWall.transform.SetParent(wallFolder.transform);
+                if (i == xMin)
+                {
+                    topWall.GetComponent<SpriteRenderer>().sprite = topLeftCornerWallSprite;
+                    topWall.GetComponent<SpriteRenderer>().sortingOrder = 1;
+                }
+                else
+                {
+                    topWall.GetComponent<SpriteRenderer>().sprite = topWallSprite;
+                }
             }
             
             for(float i = yMin; i < yMax; i++)
             {
-                Instantiate(wallPrefab, new Vector3(xMin, i, 0), Quaternion.identity).transform.SetParent(wallFolder.transform);
-                Instantiate(wallPrefab, new Vector3(xMax, i, 0), Quaternion.identity).transform.SetParent(wallFolder.transform);
+                GameObject leftWall = Instantiate(wallPrefab, new Vector3(xMin, i, 0), Quaternion.identity);
+                leftWall.transform.SetParent(wallFolder.transform);
+                leftWall.GetComponent<SpriteRenderer>().sprite = leftWallSprite;
+                GameObject rightWall = Instantiate(wallPrefab, new Vector3(xMax, i, 0), Quaternion.identity);
+                rightWall.transform.SetParent(wallFolder.transform);
+                rightWall.GetComponent<SpriteRenderer>().sprite = rightWallSprite;
             }
-            
-            Instantiate(wallPrefab, new Vector3(xMax, yMax, 0), Quaternion.identity).transform.SetParent(wallFolder.transform);
+
+            GameObject topRightCornerWall = Instantiate(wallPrefab, new Vector3(xMax, yMax, 0), Quaternion.identity);
+            topRightCornerWall.transform.SetParent(wallFolder.transform);
+            topRightCornerWall.GetComponent<SpriteRenderer>().sprite = topRightCornerWallSprite;
+            topRightCornerWall.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+            GameObject bottomRightCornerWall = Instantiate(wallPrefab, new Vector3(xMax, yMin, 0), Quaternion.identity);
+            bottomRightCornerWall.transform.SetParent(wallFolder.transform);
+            bottomRightCornerWall.GetComponent<SpriteRenderer>().sprite = bottomRightCornerWallSprite;
+            bottomRightCornerWall.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
         }
     }
 }
